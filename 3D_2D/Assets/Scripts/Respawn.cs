f511b2ec6932132b7c55b5a671dc1d1612d79f1a
m1ttt <68821845+m1ttt@users.noreply.github.com>
2022-09-22 02:16:29 -0500

    Respawn

---
+++ 3D_2D/Assets/Scripts/Respawn.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+
+    private Vector2 respawn;
+    public GameObject detectordeCaida;
+    // Start is called before the first frame update
+    void Start()
+    {
+        respawn = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        detectordeCaida.transform.position = new Vector2(transform.position.x, detectordeCaida.transform.position.y);
+
+
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "DetectorDeCaida")
+        {
+            transform.position = respawn;
+        }
+    }
+}
